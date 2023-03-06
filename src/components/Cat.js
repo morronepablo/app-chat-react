de34a71d7970d949a7d87d96e63552a326557537
morronepablo <morronepablo@gmail.com>
2023-03-05 21:03:16 -0300

    subiendo proyecto

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src="https://images.pexels.com/photos/1404819/pexels-photo-1404819.jpeg" />
+  )
+}
+
+export default Cat
---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chats from "./Chats";
+
+const GeneralApp = () => {
+
+  return (
+    <>
+      {/* Chats */}
+      <Chats />
+    </>
+  );
+};
+
+export default GeneralApp;
